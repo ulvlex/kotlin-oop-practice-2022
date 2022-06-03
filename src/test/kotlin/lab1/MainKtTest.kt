058c14237c223f9c9dee25fddf8db8e987ddeabd
ulvlex <ulyanaspr@mail.ru>
2022-06-03 15:58:51 +0300

    Lab1_tests

--- src/test/kotlin/lab1/MainKtTest.kt
+++ src/test/kotlin/lab1/MainKtTest.kt
@@ -1,19 +1,22 @@
 package lab1
 
-import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 internal class MainKtTest {
     @Test
     fun bookParserTest1() {
         //first
-        val books1 = "1.Солярис//Станислав Лем//1961\n" +
+        val books1 =  "1.Солярис//Станислав Лем//1961\n" +
                 "2.Три сестры//Антон Чехов//1900\n" +
                 "3.С++ Primer//Стэнли Липпман, Жозе Лажуа и Барбара Э. Му//2012"
         val bookList: List<Book> = parseBooks(books1)
-        assert(bookList[0].name == "Солярис" && bookList[0].authors == "Станислав Лем" && bookList[0].pubDate == 1961)
-        assert(bookList[1].name == "Три сестры" && bookList[1].authors == "Антон Чехов" && bookList[1].pubDate == 1900)
-        assert(bookList[2].name == "С++ Primer" && bookList[2].authors == "Стэнли Липпман, Жозе Лажуа и Барбара Э. Му" && bookList[2].pubDate == 2012)
+        val expected = listOf(
+            Book("Солярис", "Станислав Лем", 1961),
+            Book("Три сестры", "Антон Чехов", 1900),
+            Book("С++ Primer", "Стэнли Липпман, Жозе Лажуа и Барбара Э. Му", 2012)
+        )
+        assertEquals(expected, bookList)
     }
 
     @Test
@@ -21,7 +24,10 @@ internal class MainKtTest {
         //second
         val books2 = "1.Солярис//Станислав Лем//1961"
         val bookList2: List<Book> = parseBooks(books2)
-        assert(bookList2[0].name == "Солярис" && bookList2[0].authors == "Станислав Лем" && bookList2[0].pubDate == 1961)
+        val expected = listOf(
+            Book("Солярис", "Станислав Лем", 1961)
+        )
+       assertEquals(expected, bookList2)
     }
 
     @Test
@@ -32,8 +38,11 @@ internal class MainKtTest {
                 "3.С++ Primer//Стэнли Липпман, Жозе Лажуа и Барбара Э. Му//2012"
 
         val bookList3: List<Book> = parseBooks(books3)
+        val expected = listOf(
+            Book("Солярис", "Станислав Лем", 1961),
+            Book("С++ Primer", "Стэнли Липпман, Жозе Лажуа и Барбара Э. Му", 2012)
+        )
         assert(bookList3.count() == 2)
-        assert(bookList3[0].name == "Солярис" && bookList3[0].authors == "Станислав Лем" && bookList3[0].pubDate == 1961)
-        assert(bookList3[1].name == "С++ Primer" && bookList3[1].authors == "Стэнли Липпман, Жозе Лажуа и Барбара Э. Му" && bookList3[1].pubDate == 2012)
+        assertEquals(expected, bookList3)
     }
 }
